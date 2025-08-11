1882bce3b6b07c935c543fa839d4d14c9e042fde
Platinum1985 <o9annisyan.gayane@yandex.ru>
2025-08-11 18:29:03 +0300

    Add files via upload

---
+++ Final_Project_TZ_4/src/model/Epic.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Epic extends Task {
+    public ArrayList<Integer> subTaskIds = new ArrayList<>();
+
+
+    public Epic(String name, String description, Status st, int id) {
+        super(name, description, st, id);
+    }
+
+    public void addSubTaskId(int subTaskId) {
+
+        subTaskIds.add(subTaskId);
+    }
+
+
+    @Override
+    public String toString() {
+        return "model.Epic{" +
+                "name='" + this.getName() + '\'' +
+                ", description='" + this.getDescription() + '\'' +
+                ", st=" + this.getSt() +
+                ", id=" + this.getId() +
+                ", subTaskIds=" + subTaskIds +
+                '}';
+    }
+}
