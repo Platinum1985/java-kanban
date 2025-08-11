e261ebba8df4aeefb2d556237fd0ab7319042a52
Platinum1985 <o9annisyan.gayane@yandex.ru>
2025-08-11 13:34:16 +0300

    Add files via upload

---
+++ Final_Project_TZ_4  Исправленный вариант/src/manager/TaskManager.java
@@ -0,0 +1,157 @@
+package manager;
+
+import model.Epic;
+import model.SubTask;
+import model.Task;
+import model.Status;
+import java.util.HashMap;
+
+public class TaskManager {
+   public int idTask = 0;// Счётчик идентификаторов
+  public   int idEpic = 0;
+  public   int idSubTask = 0;
+  public   HashMap<Integer, Task> tasks = new HashMap(); // Список задач
+  public   HashMap<Integer, Epic> epics = new HashMap();
+   public HashMap<Integer, SubTask> subTasks = new HashMap<>();
+
+
+  public   void addTask(int i, Task task) {
+        tasks.put(i, task);
+        idTask++;
+
+    }
+
+   public void addEpics(Integer i, Epic epic) {
+        epics.put(i, epic);
+        idEpic++;
+    }
+
+ public   void addSubTask(int subTaskId, SubTask subTask, int epicId) {
+        subTasks.put(subTaskId, subTask);
+        Epic epic = epics.get(epicId);
+        if (epic != null) {
+            epic.addSubTaskId(subTaskId);
+        }
+        idSubTask++;
+    }
+
+   public String checkStatusEpic(Epic e) { //получение статуса эпика
+        int size = e.subTaskIds.size();
+        int countProgress = 0;
+        int countDone = 0;
+        for (int i : e.subTaskIds) {
+            if (subTasks.get(i).getSt().equals(Status.IN_PROGRESS) ||
+                    subTasks.get(i).getSt().equals(Status.DONE)) {
+                countProgress++;
+            }
+            if (subTasks.get(i).getSt().equals(Status.DONE)) {
+                countDone++;
+            }
+        }
+        if (countDone == size) {
+            e.setSt(Status.DONE);
+        } else if (countDone < size && countProgress > 0) {
+            e.setSt(Status.IN_PROGRESS);
+        } else {
+            e.setSt(Status.NEW);
+        }
+return e.getSt().toString();
+    }
+
+   public void clearTask(HashMap<Integer, Task> hashMap) {
+        hashMap.clear();
+    }
+
+    public void clearEpics() {
+        epics.clear();
+        subTasks.clear(); //если эпиков нет, то и подзадачи ненужны
+
+    }
+
+    void clearSubTasks() {
+        for (Epic e : epics.values())
+            e.subTaskIds.clear();
+        subTasks.clear();
+
+    }
+
+   public String printAllTasks() {
+        String result = "";
+        for (Task t : tasks.values()) {
+            result += t.toString() + "\n";
+        }
+        return result;
+
+    }
+
+   public String printAllEpics() {
+        String result = "";
+        for (Epic e : epics.values()) {
+            result += e.toString() + "\n";
+        }
+        return result;
+
+    }
+
+  public   String printAllSubTasks() {
+        String result = "";
+        for (SubTask st : subTasks.values()) {
+            result += st.toString() + "\n";
+        }
+        return result;
+    }
+
+   public String getTaskByID(int id) {
+        return tasks.get(id).toString();
+    }
+
+   public String getEpicByID(int id) {
+        return epics.get(id).toString();
+    }
+
+   public String getSubTaskByID(int id) {
+        return subTasks.get(id).toString();
+    }
+
+   public void updateTask(Task newTask, int id) {
+        tasks.put(id, newTask);
+
+    }
+
+   public void updateEpic(Epic newEpic, int id) {
+        epics.put(id, newEpic);
+
+    }
+
+   public void updateSubTask(SubTask newST, int id) {
+        subTasks.put(id, newST);
+        for (Epic e : epics.values()) {
+            for(int j:e.subTaskIds) {
+                if(j==id)
+                    this.checkStatusEpic(e);
+            }
+
+        }
+
+
+    }
+
+   public void deleteTask(int id) {
+        tasks.remove(id);
+    }
+
+   public void deleteEpic(int id) {
+        for (int j:epics.get(id).subTaskIds)//удаляем подзадач эпика из HashMap
+        {
+            subTasks.remove(j);
+        }
+        epics.remove(id);
+    }
+   public void deleteSubTask(int id) {
+        for (Epic e : epics.values()) {
+            e.subTaskIds.removeIf(j -> j == id);
+            this.checkStatusEpic(e);
+        }
+        subTasks.remove(id);
+    }
+}
---
+++ Final_Project_TZ_4  Исправленный вариант/src/model/Task.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.util.Objects;
+
+public class Task {
+ private String  name;
+ private String description; //"Описание"
+ private Status st= Status.NEW;
+ private int id;
+
+  public Task(String name, String description, Status st, int id) {
+    this.name = name;
+    this.description = description;
+    this.st = st;
+    this.id = id;
+  }
+int getId () {
+    return this.id;
+}
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    Task task = (Task) o;
+    return id == task.id;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
+  }
+
+  @Override
+  public String toString() {
+    return "model.Task{" +
+            "name='" + name + '\'' +
+            ", description='" + description + '\'' +
+            ", st=" + st +
+            ", id=" + id +
+            '}';
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public Status getSt() {
+    return st;
+  }
+
+    public void setSt(Status st) {
+        this.st = st;
+    }
+}
